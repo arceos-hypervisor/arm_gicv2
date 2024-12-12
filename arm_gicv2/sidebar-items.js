5045206ea798b079a7e8e123655f03e0224bef45
hky1999 <976929993@qq.com>
2024-12-12 08:25:54 +0000

    Deploying to gh-pages from @ arceos-hypervisor/arm_gicv2@eee14941d490719f6689e82f9a87caea6767bdc3 🚀

---
+++ arm_gicv2/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["GICC_CTLR_EN_BIT","GICC_CTLR_EOIMODENS_BIT","GIC_CONFIG_BITS","GIC_MAX_IRQ","PPI_RANGE","SGI_RANGE","SPI_RANGE"],"enum":["InterruptType","TriggerMode"],"fn":["translate_irq"],"struct":["GicCpuInterface","GicDistributor"]};
---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["arm_gicv2",["",[["regs",[],["gicd_sgir.rs","mod.rs"]]],["gic_v2.rs","lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[81]}
